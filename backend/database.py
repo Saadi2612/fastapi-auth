1f137dd83f5aefeb306281055f8b1490f9bd593f
Muhammad Saad Hasan <saadhassan817@gmail.com>
2024-07-11 23:53:20 +0500

    fastAPI auth app

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:##Rsshham@localhost:5432/TestDB"
+database_url = os.getenv("DATABASE_URL")
+
+SQLALCHEMY_DATABASE_URL = database_url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
